ad670ae3eec47c4f5163f1a035bbca0fa04a3f9f
bmchun <bianmengchun@qq.com>
2015-12-15 17:41:54 +0800

    add

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -8,7 +8,7 @@
 class IndexController extends Yaf_Controller_Abstract {
 
 	/** 
-     * 默认动作
+     * 默认动作aa
      * Yaf支持直接把Yaf_Request_Abstract::getParam()得到的同名参数作为Action的形参
      * 对于如下的例子, 当访问http://yourhost/mengchun.com/index/index/index/name/root 的时候, 你就会发现不同
      */
